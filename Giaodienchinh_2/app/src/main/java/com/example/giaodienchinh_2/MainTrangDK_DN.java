5736ebe518f8f51d821685f64cbf34b70a3cd88d
NguyenTrongPhuoc-CDTH18PMC <0306181260@caothang.edu.vn>
2020-11-25 23:27:35 +0700

    test

--- Giaodienchinh_2/app/src/main/java/com/example/giaodienchinh_2/MainTrangDK_DN.java
+++ Giaodienchinh_2/app/src/main/java/com/example/giaodienchinh_2/MainTrangDK_DN.java
@@ -35,7 +35,7 @@ public class MainTrangDK_DN extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_trang_d_k__d_n);
-        initView();
+        ////initView();
 
 
 
